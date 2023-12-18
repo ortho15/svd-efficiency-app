cd1290b5556899c62652a79c456af25ec435e5f6
Jimmy Choi <example@example.com>
2023-12-17 23:47:37 -0500

    initial

---
+++ func.py
@@ -0,0 +1,56 @@
+import numpy as np
+import math
+from PIL import Image
+
+cache_eigens = {}
+
+def jpg_to_RGB(file):
+    img = Image.open(file)
+    R = np.array(img)[:,:,0]
+    G = np.array(img)[:,:,1]
+    B = np.array(img)[:,:,2]
+
+    return R,G,B
+
+def eigens(R,G,B):
+    global cache_eigens
+    three = {
+            'R': R,
+            'G': G,
+            'B': B
+            }
+    for name, A in three.items():
+        AtA = np.dot(A.T, A)
+        if not np.allclose(AtA, AtA.T, rtol=1e-05, atol=1e-08):
+            return "AtA is not symmetric!"
+        eigenValues, eigenVectors = np.linalg.eigh(AtA)
+        idx = eigenValues.argsort()[::-1]
+        eigenValues = eigenValues[idx]
+        eigenVectors = eigenVectors[:,idx]
+        neg_idx = next((index for index, value in enumerate(eigenValues) if value < 0), None)
+        eigenValues = eigenValues[:neg_idx]
+        eigenVectors = eigenVectors[:neg_idx]
+        eigenValues = [math.sqrt(x) for x in eigenValues]
+        cache_eigens[name] = (eigenValues, eigenVectors)
+
+    return cache_eigens # they are singular values now
+
+def compress(r, three):
+    U = np.empty((three['R'].shape[0], three['R'].shape[0]))
+    for color, V in cache_eigens.items():
+        for i in range(r):
+            print(cache_eigens[color][1][:[i]].shape)
+            # col = (1/cache_eigens[color][0][i]) * np.dot(three[color], cache_eigens[color][1][:i])
+            return "ok"
+            U = np.insert(U, i, col, axis=1)
+    return u
+
+
+def matrix_to_img(R,G,B):
+    full = np.zeros((R.shape[0], R.shape[1], 3), dtype=np.uint8)
+    full[:, :, 0] = R
+    full[:, :, 1] = G
+    full[:, :, 2] = B
+    img = Image.fromarray(full)
+    img.save('./assets/reduced.png')
+    return './assets/reduced.png'
---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import func
+
+# Webpage Config
+st.set_page_config(page_title='Image SVD Effiency Demonstration')
+#, page_icon = favicon, layout = 'wide', initial_sidebar_state = 'auto')
+
+# Globals
+image = "./assets/main.jpg"
+
+# Body
+
+st.markdown("# Image SVD Effiency Demonstration")
+
+st.markdown("## Original Image")
+st.image(image)
+
+st.markdown("## After SVD")
+
+R,G,B = func.jpg_to_RGB(image)
+three = {
+    'R': R,
+    'G': G,
+    'B': B
+}
+og_size = R.shape
+
+# CORE
+l, r = st.columns([0.8, 0.2])
+
+# l.image(reduced)
+r.write(f"""matrix_to_img
+Original size: {og_size}
+""")
+
+rbg_eigens = func.eigens(R,G,B) # number of singular values ordered from biggest to smallest
+rank = min(len(rbg_eigens['R'][0]), len(rbg_eigens['G'][0]), len(rbg_eigens['B'][0]))
+start = rank-20
+chosen = st.slider("rank", 1, rank, start, 10)
+u = func.compress(chosen, three)
+
+# TESTING!!!
+st.write(u)
+# TESTING!!!
